31a7b7b9babff6d5ee4f374c41d7a5a83ac98934
jpsoria6 <jpsoria6@hotmail.com>
2024-08-28 02:45:46 -0300

    add empty message when documents are empty

--- src/app/components/ui/DocsList.tsx
+++ src/app/components/ui/DocsList.tsx
@@ -1,11 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import {
-  Grid,
-  Tab,Box, Tabs
-} from "@mui/material";
+import { Grid, Tab, Box, Tabs, Typography } from "@mui/material";
 import { useDocs } from "@/app/hooks/useDocs";
-import DocSpecialListTable from "./DocSpecialListTable";
 import DataGridDocs from "./DataGridDocs";
 import DataGridSpecialDocs from "./DataGridSpecialDocs";
 
@@ -31,28 +27,25 @@ function CustomTabPanel(props: TabPanelProps) {
   );
 }
 
-
 function a11yProps(index: number) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
-
 
 export const DocsList = () => {
   const { getDocs } = useDocs();
   const [documents, setDocuments] = useState([]);
   const [specialDocuments, setSpecialDocuments] = useState([]);
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
 
   useEffect(() => {
     getDocs().then((data) => {
-      setDocuments(data.documents);
-      setSpecialDocuments(data.specialDocuments);
+      setDocuments(data.documents || []);
+      setSpecialDocuments(data.specialDocuments || []);
     });
   }, []);
-
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
@@ -62,17 +55,29 @@ export const DocsList = () => {
     <>
       <Grid item xs={12}>
         <Grid item>
-          <Box sx={{ mb:2, borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs onChange={handleChange} centered>
+          <Box sx={{ mb: 2, borderBottom: 1, borderColor: "divider" }}>
+            <Tabs onChange={handleChange} centered value={value}>
               <Tab label="Documentos" {...a11yProps(0)} />
               <Tab label="Documentos Especiales" {...a11yProps(1)} />
-          </Tabs>
+            </Tabs>
           </Box>
           <CustomTabPanel value={value} index={0}>
-            <DataGridDocs rows={documents} />
+            {documents.length > 0 ? (
+              <DataGridDocs rows={documents} />
+            ) : (
+              <Typography variant="h6" align="center">
+                No hay documentos disponibles.
+              </Typography>
+            )}
           </CustomTabPanel>
           <CustomTabPanel value={value} index={1}>
-            <DataGridSpecialDocs rows={specialDocuments} />
+            {specialDocuments.length > 0 ? (
+              <DataGridSpecialDocs rows={specialDocuments} />
+            ) : (
+              <Typography variant="h6" align="center">
+                No hay documentos especiales disponibles.
+              </Typography>
+            )}
           </CustomTabPanel>
         </Grid>
       </Grid>
