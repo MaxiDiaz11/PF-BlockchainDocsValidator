027a43decdba74548473874a96d44a53d1f39656
jpsoria6 <jpsoria6@hotmail.com>
2024-09-04 18:40:32 -0300

    changes

--- src/app/components/ui/NavBar.tsx
+++ src/app/components/ui/NavBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AppBar, Box, Button, Link, Toolbar, Typography } from "@mui/material";
 import { usePathname } from "next/navigation";
 import { UiContext } from "@/app/context";
@@ -9,6 +9,14 @@ import NextLink from "next/link";
 export const NavBar = () => {
   const { toggleSideMenu } = useContext(UiContext);
   const page = usePathname();
+
+  const [isAdmin,setIsAdmin] = useState("");
+
+  useEffect(()=> {
+    const session = sessionStorage.getItem("isAdmin");
+    if (session)
+      setIsAdmin(session);
+  },[])
 
   return (
     <AppBar>
@@ -21,7 +29,7 @@ export const NavBar = () => {
             </Link>
           </NextLink>
         </Box>
-
+        {isAdmin === "false" &&  (
         <Box sx={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
           <Box sx={{ display: { xs: "none", sm: "block" } }}>
             <NextLink href="/dashboard/generate" legacyBehavior>
@@ -30,7 +38,7 @@ export const NavBar = () => {
                   sx={{ fontSize: 16}}
                   color={page === "/dashboard/generate" ? "primary" : "info"}
                 >
-                  Generar
+                  Solicitar
                 </Button>
               </Link>
             </NextLink>
@@ -56,8 +64,34 @@ export const NavBar = () => {
             </NextLink>
           </Box>
         </Box>
-
-        <Box sx={{ width: '200px', textAlign: 'right' }}>
+        )}
+         {isAdmin === "true" &&  (
+            <Box sx={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
+              <Box sx={{ display: { xs: "none", sm: "block" } }}>
+                <NextLink href="/dashboard/admin/buscar-doc" legacyBehavior>
+                  <Link>
+                    <Button
+                      sx={{ fontSize: 16}}
+                      color={page === "/dashboard/admin/buscar-doc" ? "primary" : "info"}
+                    >
+                      Buscar Petición
+                    </Button>
+                  </Link>
+                </NextLink>
+                <NextLink href="/dashboard/admin/estadisticas" legacyBehavior>
+                  <Link>
+                    <Button
+                      sx={{ fontSize: 16}}
+                      color={page === "/dashboard/admin/estadisticas" ? "primary" : "info"}
+                    >
+                      Estadísticas
+                    </Button>
+                  </Link>
+                </NextLink>
+              </Box>
+            </Box>
+          )}
+          <Box sx={{ width: '200px', textAlign: 'right' }}>
           <Button sx={{ fontSize: 16}} onClick={toggleSideMenu}>Menú</Button>
         </Box>
       </Toolbar>
